cecfabe7ea6afad75a1c33d662628302b928712c
Iago Mosqueira <iago.mosqueira@gmail.com>
2009-02-17 18:11:03 +0000

    Added test for Bug 6

--- tests/FLStock.R
+++ tests/FLStock.R
@@ -3,7 +3,7 @@
 
 # Copyright 2003-2009 FLR Team. Distributed under the GPL 2 or later
 # Maintainer: Iago Mosqueira, Cefas
-# Last Change: 17 Feb 2009 19:01
+# Last Change: 17 Feb 2009 19:06
 # $Id:  $
 
 # Reference:
@@ -20,6 +20,7 @@ tagTest("FLStock testing ...")
 
 
 # Bug IM_20090217_1855_rec
+# http://flr-project.org/bugs/index.php?do=details&task_id=6&project=2
 #   rec(FLStock) should return an FLQuant with rec.age instead of 'all'
 checkIdentical(dimnames(rec(ple4))$age, as.character(dims(ple4)$min))
 
