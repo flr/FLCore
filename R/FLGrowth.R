658a6cb72dbd212fb645c469dc165b9487fca14c
Iago Mosqueira <iago.mosqueira@gmail.com>
2010-03-31 07:11:47 +0000

    Added FLGrowth

---
+++ R/FLGrowth.R
@@ -0,0 +1,22 @@
+# FLGrowth - «Short one line description»
+# R/FLGrowth
+
+# Copyright 2003-2008 FLR Team. Distributed under the GPL 2 or later
+# Maintainer: Iago Mosqueira, Cefas
+# $Id: FLArray.R 613 2010-03-30 09:59:23Z imosqueira $
+
+# class FLGrowth
+setClass('FLGrowth', representation('FLModel',
+   mass='FLArray'))
+
+# constructor	{{{
+setGeneric('FLGrowth', function(model, ...)
+  standardGeneric('FLGrowth'))
+
+setMethod('FLGrowth', signature(model='ANY'),
+   function(model, ...)
+     return(FLModel(model, ..., class='FLGrowth')))
+
+setMethod('FLGrowth', signature(model='missing'),
+ function(...)
+  return(FLModel(formula(NULL), ..., class='FLGrowth')))	# }}}
