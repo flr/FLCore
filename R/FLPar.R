2a145db2a65d80714fac2cafb0cd1d309f37e271
Iago Mosqueira <iago.mosqueira@gmail.com>
2011-10-25 12:39:54 +0000

    New Arith(FLPar,FLArray)

--- R/FLPar.R
+++ R/FLPar.R
@@ -481,7 +481,6 @@ setMethod("Arith", ##  "+", "-", "*", "^", "%%", "%/%", "/"
 
 setMethod("Arith", signature(e1 = "FLArray", e2 = "FLPar"),
   function(e1, e2) {
-
     # objects dims
     d1 <- dim(e1)
     d2 <- dim(e2)
@@ -489,36 +488,35 @@ setMethod("Arith", signature(e1 = "FLArray", e2 = "FLPar"),
     n1 <- names(dimnames(e1))
     n2 <- names(dimnames(e2))
 
+    # iter of output
+    it <- max(d1[6], d2[l2])
+
     # dims of length > 1 (except iter), must be in FLArray
     if(any(!(n2[-l2][d2[-l2] > 1]) %in% (n1[-6][d1[-6] > 1])))
       stop(paste("FLPar object cannot have dimensions of length > 1 not in",
         ac(class(e1))))
 
     # reshape FLPar
-    m2 <- match(n1, n2)
-    m2 <- c(1, m2[!is.na(m2)])
-    e2 <- aperm(e2, m2)
+    m2 <- unique(c(match(n2[-length(d2)], n1[-6]), seq(1, 6)))
+    m2 <- m2[!is.na(m2)]
 
-    # iter of output
-    it <- max(d1[6], d2[l2])
+    e2 <- aperm(array(e2, dim=d1[m2]), m2)
+
 
     # iters from FLQ
     if(d1[6] >= d2[l2]) {
-      return(new(class(e1), array(callGeneric(e1@.Data, array(e2, dim=c(d1[-6], it))),
-        dim=d1, dimnames=dimnames(e1)), units=units(e1)))
-    }
-    else {
+      return(new(class(e1), array(callGeneric(e1@.Data, e2), dim=d1,
+        dimnames=dimnames(e1)), units=units(e1)))
+    } else {
       return(new(class(e1), array(callGeneric(array(e1@.Data, dim=c(c(d1[-6], it))),
         array(e2, dim=c(d1[-6], it))), dim=c(d1[-6], it),
-        dimnames=c(dimnames(e1)[-6], list(iter=seq(it)))), units=units(e1))
-      )
+        dimnames=c(dimnames(e1)[-6], list(iter=seq(it)))), units=units(e1)))
     }
   }
 )
 
 setMethod("Arith", signature(e1 = "FLPar", e2 = "FLArray"),
   function(e1, e2) {
-
     # objects dims
     d1 <- dim(e1)
     d2 <- dim(e2)
@@ -526,32 +524,36 @@ setMethod("Arith", signature(e1 = "FLPar", e2 = "FLArray"),
     n1 <- names(dimnames(e1))
     n2 <- names(dimnames(e2))
 
+    # iter of output
+    it <- max(d1[l2], d1[6])
+
     # dims of length > 1 (except iter), must be in FLArray
     if(any(!(n1[-l1][d1[-l1] > 1]) %in% (n2[-6][d2[-6] > 1])))
       stop(paste("FLPar object cannot have dimensions of length > 1 not in",
         ac(class(e2))))
 
     # reshape FLPar
-    m1 <- match(n2, n1)
-    m1 <- c(1, m1[!is.na(m1)])
-    e1 <- aperm(e1, m1)
+    m1 <- unique(c(match(n1[-length(d1)], n2[-6]), seq(1, 6)))
+    m1 <- m1[!is.na(m1)]
 
-    # iter of output
-    it <- max(d1[l1], d2[6])
+    e1 <- aperm(array(e1, dim=d2[m1]), m1)
+
 
     # iters from FLQ
     if(d2[6] >= d1[l1]) {
-      return(new(class(e2), array(callGeneric(array(e1, dim=c(d2[-6], it)),
-        e2@.Data), dim=d2, dimnames=dimnames(e2)), units=units(e2)))
-    }
-    else {
-      return(new(class(e2), array(callGeneric(array(e1, dim=c(d2[-6], it)),
-        array(e2@.Data, dim=c(c(d2[-6], it)))), dim=c(d2[-6], it),
-        dimnames=c(dimnames(e2)[-6], list(iter=seq(it)))), units=units(e2))
-      )
+      return(new(class(e2), array(callGeneric(e1, e2@.Data), dim=d2,
+        dimnames=dimnames(e2)), units=units(e2)))
+    } else {
+      return(new(class(e2), array(callGeneric(e1, e2@.Data), dim=c(d2[-6], it),
+        dimnames=c(dimnames(e2)[-6], list(iter=seq(it)))), units=units(e2)))
     }
   }
-) # }}}
+)
+
+
+
+ 
+ # }}}
 
 # ab {{{
 setMethod('ab', signature(x='FLPar', model='character'),
