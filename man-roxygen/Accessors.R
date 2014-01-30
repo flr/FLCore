da20f96cc88b628cac3b8ce3b0c1c9ad7afce423
drfinlayscott <drfinlayscott@gmail.com>
2014-01-30 14:49:29 +0100

    Added documentation templates

---
+++ man-roxygen/Accessors.R
@@ -0,0 +1,8 @@
+#' @section Accesors:
+#' All slots in the class have accessor and replacement methods defined that
+#' allow retrieving and substituting individual slots.
+#'
+#' The values passed for replacement need to be of the class of that slot.
+#' A numeric vector can also be used when replacing FLQuant slots, and the
+#' vector will be used to substitute the values in the slot, but not its other
+#' attributes.
---
+++ man-roxygen/Constructors.R
@@ -0,0 +1,5 @@
+#' @section Constructor:
+#' A construction method exists for this class that can take named arguments for
+#' any of its slots. All slots are then created to match the requirements of the
+#' class validity. If an unnamed \code{FLQuant} object is provided, this is used
+#' for sizing but not stored in any slot.
