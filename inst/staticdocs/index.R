bebea04d0bfa57b3911500c57037b173089249b2
iagomosqueira <iago.mosqueira@gmail.com>
2014-06-03 16:35:21 +0530

    Added indes file for staticdocs

---
+++ inst/staticdocs/index.R
@@ -0,0 +1,13 @@
+# index.R - DESC
+# index.R
+
+# Copyright 2003-2014 FLR Team. Distributed under the GPL 2 or later
+# Maintainer: Iago Mosqueira, JRC
+# Soundtrack:
+# Notes:
+
+sd_section("Classes",
+  "FLR classes",
+  c("FLQuant",
+		"FLStock"))
+
