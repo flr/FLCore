e189f66ffcc7836b59b796d2df1382dff74e0156
jaado <jose.deoliveira@cefas.co.uk>
2015-06-25 11:24:05 +0100

    Minor edits
    Description: minor
    Examples: summary(); additional comments

--- roxygen/FLStock.R
+++ roxygen/FLStock.R
@@ -2,7 +2,7 @@
 #' 
 #' A class for modelling a fish stock.
 #' 
-#' The \code{FLStock} object contains a representation of a fish stock This
+#' The \code{FLStock} object contains a representation of a fish stock. This
 #' includes information on removals (i.e. catches, landings and discards),
 #' maturity, natural mortality and the results of an analytical assessment
 #' (i.e. estimates of abundance and removal rates).
@@ -66,23 +66,24 @@
 #' @examples
 #' 
 #' data(ple4)
+#' summary(ple4)
+#'
+#' landings(ple4)  # get the landings slot
+#' landings(ple4) <- apply(landings.n(ple4)*landings.wt(ple4),2,sum)  # assign values to the landings slot
 #' 
-#' landings(ple4) #get the landings slot
-#' landings(ple4) <- apply(landings.n(ple4)*landings.wt(ple4),2,sum)   # assign values to the landings slot
+#' discards(ple4) <- computeDiscards(ple4)  # perform same calculation as preceding apply function
 #' 
-#' discards(ple4) <- computeDiscards(ple4)
-#' 
-#' harvest(ple4) <- 'f' # set the units of the harvest slot of an FLStock object
+#' harvest(ple4) <- 'f'  # set the units of the harvest slot of an FLStock object
 #' 
 #' catch(ple4) <- computeCatch(ple4)
 #' catch(ple4) <- computeCatch(ple4, slot="all")
 #' 
-#' ple4[,1] # subset the FLStock
-#' trim(ple4, age=2:6, year=1980:1990) #trim the FLStock
+#' ple4[,1]  # subset the FLStock
+#' trim(ple4, age=2:6, year=1980:1990)  # trim the FLStock
 #' 
-#' ssb(ple4) # calculate SSB
-#' ssbpurec(ple4) # calculate SSB per recruit
+#' ssb(ple4)  # calculate SSB
+#' ssbpurec(ple4)  # calculate SSB per recruit at zero fishing mortality
 #' 
 #' biol <- as(ple4, "FLBiol")  # coerce an FLStock to an FLBiol
-#' flsr <- as.FLSR(ple4)       # initialise an FLSR object from an FLStock
+#' flsr <- as.FLSR(ple4)  # initialise an FLSR object from an FLStock
 #' 
