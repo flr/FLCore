6908764404ea3f556a195766c79df7d396f97efd
jaado <jose.deoliveira@cefas.co.uk>
2015-06-30 09:41:00 +0100

    Revert "Revert "Removed FLQuant from aliases""
    This reverts commit fa7ab451f19ff3ca903a1ff8e752efbcecf29118.

--- roxygen/FLQuants.R
+++ roxygen/FLQuants.R
@@ -22,3 +22,11 @@
 #' \link[methods]{show}, \link[base]{summary}, \link[lattice]{xyplot},
 #' \link{FLlst}, \link[base]{list}
 #' @keywords classes
+#' @examples
+#'
+#' # Compute various FLStock indicators
+#'   data(ple4)
+#'   fqs <- FLQuants(ssb=ssb(ple4), catch=catch(ple4), rec=rec(ple4), f=fbar(ple4))
+#'   summary(fqs)
+#'   xyplot(data~year|qname, fqs, type='b', scales=list(relation='free'))
+
