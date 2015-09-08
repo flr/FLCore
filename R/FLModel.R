df3e7cb9df2dd66e00cecd274668585115ba3462
Iago Mosqueira <iago@mosqueira.eu>
2015-09-08 16:29:17 +0200

    update 2 roxygen

--- R/FLModel.R
+++ R/FLModel.R
@@ -799,8 +799,26 @@ setReplaceMethod('model', signature(object='FLModel', value='formula'),
 ) # }}}
 
 # update  {{{
-if (!isGeneric("update"))
-  setGeneric('update', useAsDefault = update)
+
+#' Method update
+#' 
+#' \code{update} is a generic function for updating a model fit using the
+#' same call that generated it. Input arguments can be provided that will alter
+#' the FLModel object accordingly.
+#'
+#' @name update
+#' @aliases update,FLModel-method
+#' @docType methods
+#' @section Generic function: update(object, ...)
+#' @author The FLR Team
+#' @seealso \linkS4class{FLModel}, \link[stats]{update}
+#' @keywords methods
+#' @examples
+#' 
+#' data(nsher)
+#' nsher <- update(nsher, ssb=ssb(nsher) * 1.4)
+#'
+
 setMethod('update', signature(object='FLModel'),
   function(object, ...)
   {
