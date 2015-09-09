6393ea5e90813f11335ab36612cb0065aadd9e1c
Iago Mosqueira <iago@mosqueira.eu>
2015-09-09 11:54:08 +0200

    SRModelName 2 roxygen

--- R/SRmodels.R
+++ R/SRmodels.R
@@ -609,6 +609,28 @@ setMethod("loglAR1", signature(obs = "numeric", hat = "numeric"),
      return(res)}) # }}}
 
 # SRModelName {{{
+
+#' Method SRModelName
+#'
+#' Convenience function to identify an SR model by its formula
+#' 
+#' A supplied formula, representing a stock-recruitment relationship, is
+#' matched against the list of all models defined in \code{FLCore} (See
+#' \link{SRModels}).
+#' 
+#' If a match is found, a string character with the name of the model is
+#' returned, otherwise \code{FALSE} is obtained.
+#'
+#' @param model A formula defining the model
+#' @return \item{name}{A character string or NULL}
+#' @author FLR Team
+#' @seealso \code{\link{SRModels}}
+#' @keywords models utilities
+#' @examples
+#' 
+#' SRModelName(rec ~ a * ssb * exp(-b * ssb))
+#' 
+
 SRModelName <- function(model){
   return(switch(gsub(" ", "", as.character(as.list(model)[3])),
       "a*ssb*exp(-b*ssb)"                 = "ricker",
