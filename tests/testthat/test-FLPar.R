5d987b18c56b5be42867f68b4a3339e7fb874978
iagomosqueira <iago@mosqueira.eu>
2016-04-26 22:17:00 +0200

    testFLPar function for FLPar()

--- tests/testthat/test-FLPar.R
+++ tests/testthat/test-FLPar.R
@@ -1,85 +1,51 @@
 # test-FLPar.R - DESC
-# /test-FLPar.R
+# FLCore/tests/testthat/test-FLPar.R
 
 # Copyright 2003-2016 FLR Team. Distributed under the GPL 2 or later
 # Maintainer: Iago Mosqueira, JRC
+
+# testFLPar {{{
+testFLPar <- function(par, msg, dim, dmns) {
+
+  test_that(msg, {
+
+    # check dim
+    expect_that(dim(par), equals(dim))
+    # check dimnames
+    expect_that(dimnames(par), equals(dmns))
+    # validObject
+    expect_true(validObject(par))
+    # is("FLPar")
+    expect_s4_class(par, "FLPar")
+
+  })
+} # }}}
 
 # FLPar('missing')
 context("FLPar('missing')")
 
 # FLPar(a=1)
-test_that("FLPar(a=1) works as expected", {
-
-  res <- FLPar(a=1)
-
-  # check dim
-  expect_that(dim(res), equals(c(1,1)))
-  # check dimnames
-  expect_that(dimnames(res), equals(list(params='a', iter='1')))
-
-})
+testFLPar(FLPar(a=1), "FLPar(a=1) works as expected",
+  c(1,1), list(params="a", iter="1"))
 
 # FLPar(a=1, b=rnorm(9))
-test_that("FLPar(a=1) works as expected", {
-
-  res <- FLPar(a=1)
-
-  # check dim
-  expect_that(dim(res), equals(c(1,1)))
-  # check dimnames
-  expect_that(dimnames(res), equals(list(params='a', iter='1')))
-
-})
-
+testFLPar(FLPar(a=1, b=rnorm(9)), "FLPar(a=1, b=rnorm(9)) works as expected",
+  c(2,9), list(params=c("a", "b"), iter=ac(seq(9))))
 
 # FLPar(a=1, iter=20)
-test_that("FLPar(a=1) works as expected", {
-
-  res <- FLPar(a=1)
-
-  # check dim
-  expect_that(dim(res), equals(c(1,1)))
-  # check dimnames
-  expect_that(dimnames(res), equals(list(params='a', iter='1')))
-
-})
-
+testFLPar(FLPar(a=1, iter=20), "FLPar(a=1, iter=20) works as expected",
+  c(1,20), list(params="a", iter=ac(seq(1, 20))))
 
 # FLPar(a=1, b=rnorm(9), iter=9)
-test_that("FLPar(a=1) works as expected", {
-
-  res <- FLPar(a=1)
-
-  # check dim
-  expect_that(dim(res), equals(c(1,1)))
-  # check dimnames
-  expect_that(dimnames(res), equals(list(params='a', iter='1')))
-
-})
-
+testFLPar(FLPar(a=1, b=rnorm(9), iter=9),
+  "FLPar(a=1, b=rnorm(9), iter=9) works as expected",
+  c(2,9), list(params=c("a","b"), iter=ac(seq(9))))
 
 # FLPar(a=1, b=rnorm(9), iter=12)
-test_that("FLPar(a=1) works as expected", {
+testFLPar(FLPar(a=1, b=rnorm(9), iter=12),
+  "FLPar(a=1, b=rnorm(9), iter=12) works as expected",
+  c(2,12), list(params=c("a", "b"), iter=ac(seq(12))))
 
-  res <- FLPar(a=1)
-
-  # check dim
-  expect_that(dim(res), equals(c(1,1)))
-  # check dimnames
-  expect_that(dimnames(res), equals(list(params='a', iter='1')))
-
-})
-
-# FLPar(params=c('a','b'))
-test_that("FLPar(a=1) works as expected", {
-
-  res <- FLPar(a=1)
-
-  # check dim
-  expect_that(dim(res), equals(c(1,1)))
-  # check dimnames
-  expect_that(dimnames(res), equals(list(params='a', iter='1')))
-
-})
-
-
+# FLPar(params=c("a","b"))
+testFLPar(FLPar(params=c("a","b")), "FLPar(params=c('a','b')) works as expected",
+ c(2,1), list(params=c("a", "b"), iter=ac(seq(1))))
