25d3898a9dd166624c21ba0f157b9bf4ad5005ad
Iago Mosqueira <iago@mosqueira.eu>
2017-09-12 14:57:24 +0200

    knitr_printr method for FLQuant

--- R/FLQuant.R
+++ R/FLQuant.R
@@ -176,7 +176,6 @@ function(object, dim=rep(1,6), dimnames="missing", quant=NULL, units="NA",
     dimnames[which(dim==1)] <- list(quant='all', year=1, unit='unique', season='all',
     area='unique', iter='1')[which(dim==1)]
   }
-  
   flq <- new("FLQuant", array(as.double(object), dim=dim, dimnames=dimnames),
     units=units)
 
@@ -200,7 +199,7 @@ function(object, dim=rep(1,6), dimnames="missing", quant=NULL, units="NA",
 #' FLQuant(matrix(rnorm(12), nrow=3, ncol=3))
 
 setMethod("FLQuant", signature(object="matrix"),
-  function(object, dim="missing", dimnames="missing", ...) {
+  function(object, dim=lapply(dimnames, length), dimnames="missing", ...) {
 
   if(missing(dim))
     dim <- c(nrow(object), ncol(object), rep(1,5))[1:6]
@@ -1297,7 +1296,43 @@ setMethod("harvest", signature(object="FLQuant", catch="FLQuant"),
   }
 ) # }}}
 
-# NOT EXPORTED
+# knit_print.FLQuant{{{
+knit_print.FLQuant <- function(object, options, cols=5) {
+
+    # dims
+    do <- dim(object)
+
+    # More year than cols * 2
+    if(do[2] > (cols * 2)) {
+      scols <- 1:cols
+      ecols <- dimnames(object)[[2]][(do[2]-cols+1):do[2]]
+
+      x1 <- object[,scols]
+      x2 <- object[,ecols]
+
+      if(dim(object)[6] != 1)
+        cat("iters: ", dim(object)[6],"\n\n")
+
+      if(dim(object)[6] > 1) {
+        x1v1 <- apply(x1@.Data, 1:5, median, na.rm=TRUE)
+        x1v2 <- apply(x1@.Data, 1:5, mad, na.rm=TRUE)   
+        x1v3 <- paste(format(x1v1,digits=5),"(", format(x1v2, digits=3), ")", sep="")
+        x2v1 <- apply(x1@.Data, 1:5, median, na.rm=TRUE)
+        x2v2 <- apply(x1@.Data, 1:5, mad, na.rm=TRUE)   
+        x2v3 <- paste(format(x1v1,digits=5),"(", format(x1v2, digits=3), ")", sep="")
+      } else {
+        x1v3 <- format(x1,digits=5)
+        x2v3 <- format(x2,digits=5)
+      }
+    
+      print(array(x1v3, dim=dim(x1)[1:5], dimnames=dimnames(x1)[1:5]), quote=FALSE)
+      cat("      [ ... ", do[2] - cols*2,"years]\n\n")
+      print(array(x2v3, dim=dim(x2)[1:2], dimnames=dimnames(x2)[1:2]), quote=FALSE)
+    } else {
+      print(object)
+    }
+} # }}}
+
 # filldimnames       {{{
 filldimnames <- function(dnames, dim=rep(1,6), iter=1) {
 # check only one name for quant in input
@@ -1317,3 +1352,4 @@ names(xnames)[1] <- names(dnames)[i]
 }
 return(xnames)
 } # }}}
+
