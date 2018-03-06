a1a8f956933ad7df529e38455d49e4631c90b193
Iago Mosqueira <iago@mosqueira.eu>
2018-03-06 10:43:18 +0100

    writeADMB now accepts append=TRUE/FALSE

--- R/io.ADMB.R
+++ R/io.ADMB.R
@@ -13,35 +13,19 @@ readADMB<-function(file){
     ## name elements
     names(vals)<-lapply(grep("#",dat,value=T),function(x) substr(x,3,nchar(x)))
     return(vals)}
-  
-writeADMB<-function(x,file){
-    cat("#", names(x[1]),"\n",file=file,append=FALSE)
-    cat(x[[1]],"\n",file=file,append=TRUE)
-    for (i in 2:length(x)){
-      cat("#", names(x[i]),"\n",file=file,append=TRUE)
-      cat(x[[i]],"\n",file=file,append=TRUE)}}
-      
+ 
+# writeADMB {{{
 
-# # readADMB {{{
-# readADMB <- function(file) {
-#   
-#   # read in data from ADMB Par or Rep file
-#   dat  <-scan(file, what="", sep="\n", skip=1)
-#   # Get values
-#   vals <- lapply(strsplit(dat[grep("#", dat, invert=TRUE)], " "),
-#     function(x) as.numeric(x[nchar(x)>0]))
-#   # name elements
-#   names(vals) <- lapply(grep("#", dat, value=T),
-#     function(x) substr(x,3,nchar(x)))
-#   return(vals)
-# } # }}}
-#   
-# # writeADMB {{{
-# writeADMB <- function(x,file) {
-#   cat("#", names(x[1]), "\n",file=file, append=FALSE)
-#   cat(x[[1]], "\n", file=file, append=TRUE)
-#   
-#   for (i in 2:length(x)) {
-#     cat("#", names(x[i]), "\n", file=file, append=TRUE)
-#     cat(x[[i]], "\n", file=file, append=TRUE)}
-# } # }}}
+writeADMB <- function(x, file, append=FALSE) {
+
+  cat('#', names(x[1]),'\n', file=file, append=append)
+  cat(x[[1]], '\n', file=file, append=TRUE)
+  
+  if (length(x) > 1)
+  for (i in 2:length(x)) {
+    cat('#', names(x[i]),'\n', file=file, append=TRUE)
+    cat(x[[i]], '\n', file=file, append=TRUE)
+  }
+  invisible(TRUE)
+}
+# }}}
