baf990e82a76ecec36789c466ea49b9b1ff09f82
Iago Mosqueira <iago@mosqueira.eu>
2020-04-12 11:40:26 +0200

    expect_validclass expectation to check class and validity

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,13 +1,39 @@
 # testthat.R - DESC
 # /testthat.R
 
-# Copyright 2015 Iago Mosqueira (EC JRC) <iago.mosqueira@jrc.ec.europa.eu>
-#
-# Distributed under terms of the European Union Public Licence (EUPL) 1.1.
-#
-# Notes:
+# Copyright 2003-2020 FLR Team. Distributed under the GPL 2 or later
+# Maintainer: Iago Mosqueira (WMR) <iago.mosqueira@wur.nl>
+
 
 library(testthat)
+
+# expect_validclass {{{
+
+expect_validclass <- function(object, class) {
+  
+  # CAPTURE object and label
+  act <- quasi_label(rlang::enquo(object), arg="object")
+
+  # EXPECTATIONS
+
+  # CLASS
+  expect(
+    is(act$val, class),
+    sprintf("%s is not of class '%s'", act$lab, class)
+    )
+
+  # VALIDITY
+  act$valid <- validObject(act$val)
+  expect(
+    all(act$valid),
+    sprintf("%s is not a valid %s object", act$lab, class)
+    )
+
+  # RETURN value
+  invisible(act$val)
+} # }}}
+
+
 library(FLCore)
 
 test_check("FLCore")
